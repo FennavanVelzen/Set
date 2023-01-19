fe2fac5c1e0a5beb87f1f01b7a65cd6e75440f52
FennavanVelzen <fennavvelzen@gmail.com>
2023-01-19 20:18:49 +0100

    Functie voor het generen van kaart mogelijkheden.

--- Main.py
+++ Main.py
@@ -9,15 +9,43 @@ import pygame
 
 WIN = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Set")
-WHITE = (255, 255, 255)
+FPS = 60
 
+#RGB kleuren
+WHITE = (255, 255, 255)
+LILA = 	(220,175,255)
+BLACK = (0,0,0)
+
+"""Functie om alle combinatie mogelijkheden voor kaarten te genereren.
+Deze functie neemt steeds een eigenschap uit een lijst en voegt hier alle 
+overige mogelijkheden van combinaties van andere lijsten aan toe."""
+
+def Kaarten():
+    Kleuren = ['Rood', 'Groen', 'Paars'] #lijsten met eigenschap mogelijkheden van de kaarten
+    Vormen = ['Ruit', 'Ovaal', 'Golf']
+    Hoeveelheid = [1, 2, 3]
+    Vulling = ['leeg', 'Gestreept' , 'Vol']
+    Kaarten = []                        #lijst om alle kaart combinaties in toe te voegen
+    for kleur in Kleuren:
+        for vorm in Vormen:
+            for aantal in Hoeveelheid:
+                for inhoud in Vulling:
+                    Kaart = [kleur, vorm, aantal, inhoud]
+                    Kaarten.append(Kaart)
+        
+    return Kaarten
+
+
+"""Main funcitie die alle functies los oproept"""
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        WIN.fill(WHITE)
+        WIN.fill(LILA)
         pygame.display.update()
     pygame.quit()
 
