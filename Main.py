f13134c41ca6f8b2d59abaf986fd22a73736ed61
FennavanVelzen <fennavvelzen@gmail.com>
2023-01-22 16:16:47 +0100

    Afbeeldingen voor de vormen, en functies voor kaarten met 2 en 3 vormen toegevoegd.

--- Main.py
+++ Main.py
@@ -18,6 +18,37 @@ WHITE = (255, 255, 255)
 LILA = 	(220,175,255)
 BLACK = (0,0,0)
 
+#Alle vorm en vulling combinaties
+R_Ruit_Leeg = pygame.image.load("R_Ruit_Leeg.png")
+R_Ruit_Gestreept  = pygame.image.load("R_Ruit_Gestreept.png")
+R_Ruit_Vol  = pygame.image.load("R_Ruit_Vol.png")
+R_Golf_Leeg  = pygame.image.load("R_Golf_Leeg.png")
+R_Golf_Gestreept  = pygame.image.load("R_Golf_Gestreept.png")
+R_Golf_Vol  = pygame.image.load("R_Golf_Vol.png")
+R_Ovaal_Leeg  = pygame.image.load("R_Ovaal_Leeg.png")
+R_Ovaal_Gestreept  = pygame.image.load("R_Ovaal_Gestreept.png")
+R_Ovaal_Vol  = pygame.image.load("R_Ovaal_Vol.png")
+G_Ruit_Leeg = pygame.image.load("G_Ruit_Leeg.png")
+G_Ruit_Gestreept  = pygame.image.load("G_Ruit_Gestreept.png")
+G_Ruit_Vol  = pygame.image.load("G_Ruit_Vol.png")
+G_Golf_Leeg  = pygame.image.load("G_Golf_Leeg.png")
+G_Golf_Gestreept  = pygame.image.load("G_Golf_Gestreept.png")
+G_Golf_Vol  = pygame.image.load("G_Golf_Vol.png")
+G_Ovaal_Leeg  = pygame.image.load("G_Ovaal_Leeg.png")
+G_Ovaal_Gestreept  = pygame.image.load("G_Ovaal_Gestreept.png")
+G_Ovaal_Vol  = pygame.image.load("G_Ovaal_Vol.png")
+P_Ruit_Leeg = pygame.image.load("P_Ruit_Leeg.png")
+P_Ruit_Gestreept  = pygame.image.load("P_Ruit_Gestreept.png")
+P_Ruit_Vol  = pygame.image.load("P_Ruit_Vol.png")
+P_Golf_Leeg  = pygame.image.load("P_Golf_Leeg.png")
+P_Golf_Gestreept  = pygame.image.load("P_Golf_Gestreept.png")
+P_Golf_Vol  = pygame.image.load("P_Golf_Vol.png")
+P_Ovaal_Leeg  = pygame.image.load("P_Ovaal_Leeg.png")
+P_Ovaal_Gestreept  = pygame.image.load("P_Ovaal_Gestreept.png")
+P_Ovaal_Vol  = pygame.image.load("P_Ovaal_Vol.png")
+
+
+
 """Functie om alle combinatie mogelijkheden voor kaarten te genereren.
 Deze functie neemt steeds een eigenschap uit een lijst en voegt hier alle 
 overige mogelijkheden van combinaties van andere lijsten aan toe."""
@@ -55,6 +86,17 @@ def text(Text):
     service = font.render( Text , True, (BLACK))
     WIN.blit(service, (0, 0))
 
+"""Functies om kaarten met 2 en 3 vormen te maken, met de posities het midden 
+van de kaart"""
+def vormen2(vorm, positiex, positiey):
+    WIN.blit(vorm,(positiex, positiey + 25) )
+    WIN.blit(vorm, (positiex, positiey - 25))
+
+def vormen3(vorm, positiex, positiey):
+    WIN.blit(vorm, (positiex, positiey +50))
+    WIN.blit(vorm, (positiex, positiey))
+    WIN.blit(vorm, (positiex, positiey -50))
+    
 
 """Functies voor het maken van de 3 figuren, vol en leeg"""
 GOLF = [(300, 300), (250, 250), (200, 300), (150, 250)] #hoekputen
